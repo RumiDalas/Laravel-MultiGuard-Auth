f2b16d087c1e466ba111d09785e1b2836be2bc96
Rumi <rumidalas@gmail.com>
2021-10-21 11:19:56 -0800

    Multi Guarad Auth

---
+++ app/Http/Controllers/User/UserController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers\User;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+   public function create(Request $request){
+
+    $request->validate([
+        'name'=>'required',
+        'email'=>'required|email|unique:users,email',
+        'password'=>'required|min:6|max:15',
+        'cpassword'=>'required|same:password'
+     ],[
+         'cpassword.required'=>'The confirm field is required.',
+         'cpassword.same'=>'The confirm password and password must match.'
+     ]);
+
+     $user = new User();
+     $user->name = $request->name;
+     $user->email = $request->email;
+     $user->password = Hash::make($request->password);
+     $data = $user->save();
+
+     if($data){
+         return redirect()->back()->with('success','You have register successfully.');
+
+     }else{
+        return redirect()->back()->with('error','Registration failed.');
+     }
+
+   }
+
+   public function doLogin(Request $request){
+    $request->validate([
+        'email'=>'required|email',
+        'password'=>'required|min:6|max:15',
+     ]);
+     $check=$request->only('email','password');
+     if(Auth::guard('web')->attempt($check)){
+        return redirect()->route('user.home')->with('success','Welcome to dashboard.');
+
+     }else{
+        return redirect()->back()->with('error','Login Failed.');
+
+     }
+
+   }
+
+   public function logout(){
+    Auth::guard('web')->logout();
+    return redirect('/');
+
+   }
+
+
+}
---
+++ routes/web.php
@@ -0,0 +1,60 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\User\UserController;
+use App\Http\Controllers\Admin\AdminController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+//Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::prefix('user')->name('user.')->group(function () {
+    Route::middleware(['guest:web'])->group(function () {
+        Route::view('/login','dashboard.user.login')->name('login');
+        Route::view('/register','dashboard.user.register')->name('register');
+        Route::post('/create',[UserController::class,'create'])->name('create');
+        Route::post('/dologin',[UserController::class,'doLogin'])->name('dologin');
+        
+    });
+
+
+    Route::middleware(['auth:web'])->group(function () {
+        Route::view('/home','dashboard.user.home')->name('home');
+        Route::post('/logout',[UserController::class,'Logout'])->name('logout');
+        
+    });
+
+});
+
+
+Route::prefix('admin')->name('admin.')->group(function () {
+    Route::middleware(['guest:admin'])->group(function () {
+        Route::view('/login','dashboard.admin.login')->name('login');
+        Route::post('/dologin',[AdminController::class,'doLogin'])->name('dologin');
+        
+    });
+
+
+    Route::middleware(['auth:admin'])->group(function () {
+        Route::view('/home','dashboard.admin.home')->name('home');
+        Route::post('/logout',[AdminController::class,'Logout'])->name('logout');
+        
+    });
+
+});
+
